f15509f3b4478828831dbb144bcfd2729fa6aba2
Onur Ucar <onurucar@MacBook-Pro-3.attlocal.net>
2021-11-26 21:30:11 -0500

    Settings prototype

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,11 +1,12 @@
 import "./settings.scss"
 import Sidebar from "../../components/sidebar/Sidebar"
+import placeholderImg from "../../components/assets/me.JPG"
 
 const Settings = () => {
     return ( 
         <div className="settings">
             <div className="settingsWrapper">
-                <div className="settingsName">
+                <div className="settingsTitle">
                     <div className="settingsSelections">
                         <div className="settingsUpdateWrapper">
                             <i className="fas fa-user-edit settingIcon"></i>
@@ -15,10 +16,30 @@ const Settings = () => {
                             <i className="fas fa-user-slash settingIcon"></i>
                             <span className="settingsDeleteTitle">delete your profile</span>
                         </div>
-                        
+                    </div>
+                </div>
+                
+                {/* FORM */}
+                <form action="#" className="settingsForm">
+                    <label>profile picture</label>
+                    <div className="settingsProfilePicture">
+                        <img src={placeholderImg} alt="Profile Picture" />
+
+                        <label htmlFor="fileInput">
+                            <i className="far fa-user-circle settingProfilePicture"></i>
+                        </label>
+                        <input type="file" class="imageInput" id="fileInput" />
                     </div>
 
-                </div>
+                    {/* Username */}
+                    <label>Username</label>
+
+
+                    {/* Email */}
+
+
+                </form>
+
             </div>
             <Sidebar/>
 
